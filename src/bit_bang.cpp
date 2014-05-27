7d00548f0ef1448c7053177b28b3c23da1dd602c
Geoffrey Card <gercunderscore4@gmail.com>
2014-05-27 02:03:08 -0700

    adding SPI access functions (incomplete)

--- src/bit_bang.cpp
+++ src/bit_bang.cpp
@@ -12,6 +12,32 @@
 #include "bit_bang.h"
 
 /*
+ * do not sure this directly
+ */
+inline char transfer_byte (char si)
+{
+	char so = 0;
+	
+	// data transfer
+	for (int i = 7; i <= 0; i--) {
+		// Slave Input write
+		digitalWrite(SI, (si >> i) & 0x01);
+		// SCLK up
+		digitalWrite(SCLK, HIGH);
+		// Slave Output read
+		so |= (char) digitalRead(SO) << i;
+		// wait t_hd = 10 ns
+		NSDELAY;
+		// SCLK down
+		digitalWrite(SCLK, LOW);
+		// wait t_sd = 10 ns
+		NSDELAY;
+	}
+	
+	return so;
+}
+
+/*
  * set up CC1120 SPI
  * PARAM:
  *     none, pins are defined constants
@@ -41,7 +67,7 @@ void CC1120_setup (void)
  * RETURN:
  *     state: the current state of the transceiver
  */
-char CC1120_transfer (char strobe_mode, bool rw, char addr, int burst, char* data_in, char* data_out)
+char CC1120_transfer (bool rw, char addr, int burst, char* data_in, char* data_out)
 {
 	// all inputs and outputs
 	char si = (char) (rw << 7) | addr;
@@ -59,29 +85,9 @@ char CC1120_transfer (char strobe_mode, bool rw, char addr, int burst, char* dat
 		burst = 0;
 	}
 
-	// set mode for RX/TX strobes
-	switch (strobe_mode) {
-		case SPWD_MODE:
-			digitalWrite(SCLK, LOW);
-			digitalWrite(SI,   LOW);
-			break;
-		case STX_MODE:
-			digitalWrite(SCLK, LOW);
-			digitalWrite(SI,   HIGH);
-			break;
-		case SIDLE_MODE:
-			digitalWrite(SCLK, HIGH);
-			digitalWrite(SI,   LOW);
-			break;
-		case SRX_MODE:
-			digitalWrite(SCLK, HIGH);
-			digitalWrite(SI,   HIGH);
-			break;
-		default:
-			digitalWrite(SCLK, LOW);
-			digitalWrite(SI,   LOW);
-			break;
-	}
+	// set inputs low
+	digitalWrite(SCLK, LOW);
+	digitalWrite(SI,   LOW);
 	// set CSn 0
 	digitalWrite(CSn,  LOW);
 
@@ -159,6 +165,197 @@ char CC1120_transfer (char strobe_mode, bool rw, char addr, int burst, char* dat
 		}
 	}
 	
+	// wait t_ns = 200 ns
+	NSDELAY;
+	NSDELAY;
+	NSDELAY;
+	NSDELAY;
+	
+	// set CSn high
+	digitalWrite(CSn, HIGH);
+	
+	return state;
+}
+
+/*
+ * transfer data with the CC1120, read and write
+ * PARAM:
+ *     rw: read/write, 0 for write, 1 for read
+ *     addr: six bit address of data
+ *     data_in: data to write, recommend NULL in read mode
+ *     data_out: address for writing slave output, recommend NULL in write mode
+ * RETURN:
+ *     state: the current state of the transceiver
+ */
+char SPI_single_access_in (char addr, char data_in)
+{
+	// all inputs and outputs
+	char si = (char) WRITE | addr;
+	char state = 0;
+	
+	// set inputs low
+	digitalWrite(SCLK, LOW);
+	digitalWrite(SI,   LOW);
+	// set CSn 0
+	digitalWrite(CSn,  LOW);
+
+	// wait t_sp = 50 ns
+	NSDELAY;
+
+	// address
+	state = transfer_byte(si);
+
+	// wait between bytes, t_cl + t_sd = 70
+	NSDELAY;
+	NSDELAY;
+
+	// data transfer
+	state = transfer_byte(data_in);
+
+	// wait t_ns = 200 ns
+	NSDELAY;
+	NSDELAY;
+	NSDELAY;
+	NSDELAY;
+	
+	// set CSn high
+	digitalWrite(CSn, HIGH);
+	
+	return state;
+}
+
+/*
+ * transfer data with the CC1120, read and write
+ * PARAM:
+ *     rw: read/write, 0 for write, 1 for read
+ *     addr: six bit address of data
+ *     data_in: data to write, recommend NULL in read mode
+ *     data_out: address for writing slave output, recommend NULL in write mode
+ * RETURN:
+ *     state: the current state of the transceiver
+ */
+char SPI_single_access_out (char addr, char* data_out)
+{
+	// all inputs and outputs
+	char si = (char) READ | addr;
+	char state = 0;
+	
+	// set inputs low
+	digitalWrite(SCLK, LOW);
+	digitalWrite(SI,   LOW);
+	// set CSn 0
+	digitalWrite(CSn,  LOW);
+
+	// wait t_sp = 50 ns
+	NSDELAY;
+
+	// address
+	state = transfer_byte(si);
+
+	// wait between bytes, t_cl + t_sd = 70
+	NSDELAY;
+	NSDELAY;
+	// data transfer
+	(*data_out) = transfer_byte(NULL);
+
+	// wait t_ns = 200 ns
+	NSDELAY;
+	NSDELAY;
+	NSDELAY;
+	NSDELAY;
+	
+	// set CSn high
+	digitalWrite(CSn, HIGH);
+	
+	return state;
+}
+
+/*
+ * transfer data with the CC1120, read and write
+ * PARAM:
+ *     rw: read/write, 0 for write, 1 for read
+ *     addr: six bit address of data
+ *     data_in: data to write, recommend NULL in read mode
+ *     data_out: address for writing slave output, recommend NULL in write mode
+ * RETURN:
+ *     state: the current state of the transceiver
+ */
+char SPI_burst_access_in (char addr, char** data_in)
+{
+	// all inputs and outputs
+	char si = (char) WRITE | addr;
+	char state = 0;
+	
+	// set inputs low
+	digitalWrite(SCLK, LOW);
+	digitalWrite(SI,   LOW);
+	// set CSn 0
+	digitalWrite(CSn,  LOW);
+
+	// wait t_sp = 50 ns
+	NSDELAY;
+
+	// address
+	state = transfer_byte(si);
+
+	// write all the bytes
+	for (int j = 0; j < burst; j++) {
+		// wait between bytes, t_cl + t_sd = 70
+		NSDELAY;
+		NSDELAY;
+		// data transfer
+		state = transfer_byte(data_in[j]);
+	}
+
+	// wait t_ns = 200 ns
+	NSDELAY;
+	NSDELAY;
+	NSDELAY;
+	NSDELAY;
+	
+	// set CSn high
+	digitalWrite(CSn, HIGH);
+	
+	return state;
+}
+
+/*
+ * transfer data with the CC1120, read and write
+ * PARAM:
+ *     rw: read/write, 0 for write, 1 for read
+ *     addr: six bit address of data
+ *     data_in: data to write, recommend NULL in read mode
+ *     data_out: address for writing slave output, recommend NULL in write mode
+ * RETURN:
+ *     state: the current state of the transceiver
+ */
+char SPI_single_access_out (char addr, char size char** data_out)
+{
+	// all inputs and outputs
+	char si = (char) READ | addr;
+	char state = 0;
+	
+	// set inputs low
+	digitalWrite(SCLK, LOW);
+	digitalWrite(SI,   LOW);
+	// set CSn 0
+	digitalWrite(CSn,  LOW);
+
+	// wait t_sp = 50 ns
+	NSDELAY;
+
+	// address
+	state = transfer_byte(si);
+
+	// read all the bytes
+	for (int j = 0; j < burst; j++) {
+		// wait between bytes, t_cl + t_sd = 70
+		NSDELAY;
+		NSDELAY;
+		// data transfer
+		(*data_out)[j] = transfer_byte(NULL);
+	}
+
 	// wait t_ns = 200 ns
 	NSDELAY;
 	NSDELAY;
