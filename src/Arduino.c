cfafadc52458ee0a32c3399c5196a55b110b99b8
Geoffrey Card <gercunderscore4@gmail.com>
2014-05-31 12:13:44 -0700

    Create Arduino.c

---
+++ src/Arduino.c
@@ -0,0 +1,25 @@
+/*
+ * FILE:    Arduino.h // not really
+ * PROGRAM: CC1120
+ * PURPOSE: pretend arduino functions so that my code will compile
+ * AUTHOR:  Geoffrey Card
+ * DATE:    2014-06-01 - 
+ * NOTES:   I should find the real Arduino.h
+ */
+
+#include "Arduino.h"
+
+void pinMode (uint8_t pin, uint8_t mode)
+{
+	return;
+}
+
+void digitalWrite (uint8_t pin, uint8_t val)
+{
+	return;
+}
+
+int digitalRead (uint8_t pin)
+{
+	return 0;
+}
